f32eb33ba3787b3a3d6bdc2340148b3579f1e82e
rohit sharma <rohitsharma7a11@gmail.com>
2023-09-08 22:45:26 +0530

    fix

--- src/app/components/document-details/document-details.component.ts
+++ src/app/components/document-details/document-details.component.ts
@@ -9,25 +9,12 @@ import { DocumentService } from 'src/app/core/services/document.service';
   styleUrls: ['./document-details.component.css']
 })
 export class DocumentDetailsComponent {
+  data:any;
+
   constructor(private documentService: DocumentService, private activatedRoute:ActivatedRoute, private router:Router) {
-
-    this.activatedRoute.params.subscribe((params: any) => {
-      let id = params.id
-      console.log(id, "parma data")
-
-      if(!id){
-        this.router.navigate(['']);
-        return;
-      }
-
-
-      this.documentService.getById(id).subscribe((rec: any) => {
-        console.log(rec.body)
-        this.data = rec
-      })
-
+    this.activatedRoute.data.subscribe((rec:any)=>{
+      this.data = rec.docResolver
     })
    }
    
-  data:any;
 }
